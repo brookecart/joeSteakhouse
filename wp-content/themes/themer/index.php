6faf4101633ad08740df1e76c2a4e999933f9b96
brookecart <brooke.bahn@gmail.com>
2017-03-30 13:37:52 -0700

    index

--- wp-content/themes/themer/index.php
+++ wp-content/themes/themer/index.php
@@ -5,6 +5,26 @@
 		<section>
 
 		<!-- <h1><?php the_title(); ?></h1> -->
+            
+        <!-- homepage image -->
+            <div class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active" style="display: block; width: 100%; height: auto;">
+                  <img class="d-block img-fluid" src="http://localhost:8080/wp-content/uploads/2017/03/home-photo-min.png" alt="First slide" style="object-fit: contain;">
+                  <div class="carousel-caption d-none d-md-block" style="top: 45%; bottom: auto;">
+                   <h3>Joe's Steakhouse</h3>
+                    <p>and Grill</p>
+                  </div>
+                </div>
+              </div>
+            </div> 
+        <!-- end homepage image -->
+            
+        <!-- navbar -->
+            
+            
+            
+        <!-- end navbar -->    
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
